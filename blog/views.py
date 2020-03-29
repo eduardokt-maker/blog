49f43d3220cb5fc8d4c11ff65162a379fa306778
Your Name <you@example.com>
2020-03-29 20:01:45 -0300

    template

--- blog/views.py
+++ blog/views.py
@@ -1,7 +1,15 @@
 from django.shortcuts import render
+from .models import Postagem
+from django.utils import timezone
 
 # Create your views here.
 
 def post_list(request):
-    return render(request, 'blog/post_list.html', {})
+    posts= Postagem.objects.all()
+    return render(request, 'blog/post_list.html', {'posts':posts})
+
+    # return render(request, 'blog/post_list.html', {})
+
+
+
 
