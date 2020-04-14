5125357afa6320ef8fac74eea254961d1091f591
Your Name <you@example.com>
2020-04-14 20:35:13 -0300

    add orm

--- blog/views.py
+++ blog/views.py
@@ -5,8 +5,8 @@ from django.utils import timezone
 # Create your views here.
 
 def post_list(request):
-    posts= Postagem.objects.all()
-    return render(request, 'blog/post_list.html', {'posts':posts})
+    posts= Postagem.objects.filter(data_publicacao__lte=timezone.now()).order_by('data_publicacao')
+    return render(request, 'blog/post_list.html', {'poste':posts})
 
     # return render(request, 'blog/post_list.html', {})
 
