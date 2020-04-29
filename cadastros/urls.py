4b3366f5574d40c9f45dd3ea4b98bc5fa8d7530c
Your Name <you@example.com>
2020-04-29 17:44:09 -0300

    add ListView pessoal

--- cadastros/urls.py
+++ cadastros/urls.py
@@ -1,6 +1,8 @@
 from django.urls import path
 from .views import PessoasCreate
+from .views import PessoasListView
 
 urlpatterns = [
-    path('cad', PessoasCreate.as_view(), name='add_person')
+    path('cad', PessoasCreate.as_view(), name='add_person'),
+    path('personlist', PessoasListView.as_view(), name='list_person')
 ]
--- cadastros/views.py
+++ cadastros/views.py
@@ -1,5 +1,7 @@
 from .models import Pessoas
+from  django.utils import timezone
 from django.views.generic.edit import CreateView
+from django.views.generic.list import ListView
 from django.urls import reverse_lazy
 
 # Create your views here.
@@ -7,3 +9,11 @@ class PessoasCreate(CreateView):
     model = Pessoas
     fields = ['codigo', 'nome', 'rg', 'expedidor', 'uf', 'cpf', 'titulo', 'nacionalidade', 'natural', 'estado_civil', 'qtd_dependentes', 'rua', 'numero', 'complemento', 'bairro', 'cep', 'cidade', 'estado', 'celular1', 'celular2', 'e_mail' ]
     success_url = reverse_lazy('post_list')
+
+class PessoasListView(ListView):
+    model = Pessoas
+
+    def get_context_data(self,**kwargs):
+        context= super().get_context_data(**kwargs)
+        context['now'] = timezone.now()
+        return context
